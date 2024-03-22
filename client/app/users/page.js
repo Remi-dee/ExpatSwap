509213d749b0e61ac2b0f0b49185e9531a8284de
Remi-dee <oluwaremidaniel36@gmail.com>
2024-03-22 14:56:08 +0100

    Updated

--- client/app/users/page.js
+++ client/app/users/page.js
@@ -1,11 +1,14 @@
 "use client";
-import { useRouter } from "next/navigation";
+
 import UserTable from "../components/UserTableScreen";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UserTable />
+      <Suspense>
+        <UserTable />
+      </Suspense>
     </main>
   );
 }
